01319692e54083e35b9f5b5cac119833798bc531
Kirtan Desai <53828181+kirtan-desai@users.noreply.github.com>
2020-06-08 19:39:16 +0530

    Added login and logout functionality

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -3,6 +3,7 @@ from flaskblog import app, db, bcrypt
 from flaskblog.form import RegistrationForm, LoginForm
 import email_validator
 from flaskblog.model import User, Post
+from flask_login import login_user, current_user, logout_user
 
 posts = [
     {
@@ -31,6 +32,8 @@ def about():
 
 @app.route('/register', methods=['GET','POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -43,11 +46,20 @@ def register():
 
 @app.route('/login', methods=['GET','POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'pass':
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
             flash('You have successfully logged in!', 'success')
             return redirect(url_for('home'))
         else:
-            flash('The username or password is incorrect. Please try again','danger')
+            flash('The email or password is incorrect. Please try again','danger')
     return render_template("login.html", form = form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
